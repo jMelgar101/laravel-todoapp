4fabb8b9c58471a4770538ea267bb449024064e1
Julius <mjuliusvincent@gmail.com>
2023-02-23 11:07:32 +0800

    enable db user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,11 +16,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory(10)->create();
+        User::factory()->create([
+            'name' => 'Julius',
+            'email' => 'julius_melgar@spice-factory.ph',
+        ]);
     }
 }
